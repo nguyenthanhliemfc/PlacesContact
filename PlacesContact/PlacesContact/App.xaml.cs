82e35ecf566eaf384d736c6b05d6fff3f14f950a
Nguyen Liem <nguyenthanhliemfc@gmail.com>
2019-03-07 21:40:41 +0700

    Fix location on Xiao Mi Phone
    Fix location on Xiao Mi Phone

---
+++ PlacesContact/PlacesContact/App.xaml.cs
@@ -0,0 +1,36 @@
+﻿using PlacesContact.Models;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Matcha.BackgroundService;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace PlacesContact
+{
+    public partial class App : Application
+    {        
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            
+        }
+
+        protected override void OnSleep()
+        {
+            BackgroundAggregatorService.StopBackgroundService();
+            BackgroundAggregatorService.Add(() => new BackgroundJobs(3));
+            BackgroundAggregatorService.StartBackgroundService();
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
