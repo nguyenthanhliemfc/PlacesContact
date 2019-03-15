1d1d6ee597e50e205d599136c31d9cac07adb7c0
Nguyen Liem <nguyenthanhliemfc@gmail.com>
2019-03-15 22:36:04 +0700

    Update find Email
    Update find Email

---
+++ PlacesContact/PlacesContact/App.xaml.cs
@@ -0,0 +1,36 @@
+﻿using PlacesContact.Models;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Matcha.BackgroundService;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace PlacesContact
+{
+    public partial class App : Application
+    {        
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            BackgroundAggregatorService.StopBackgroundService();
+            BackgroundAggregatorService.Add(() => new BackgroundTaskJob(10)); //Task run every 10s
+            BackgroundAggregatorService.StartBackgroundService();
+        }
+
+        protected override void OnSleep()
+        {
+            
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
